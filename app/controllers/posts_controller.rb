fe238041ba18435a90d1a8b126bef0f4b62d1303
yasu1224 <yasu1224x@gmail.com>
2020-04-11 20:29:24 +0900

    画像投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :content, :img, :img_cache, :remove_img).merge(user_id: current_user.id)
   end
 
 
